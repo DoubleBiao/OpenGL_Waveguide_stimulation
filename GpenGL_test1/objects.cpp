01a54721d24d5645b7cc41daee1c89f6672de9ee
Timothy Hu <648194212@qq.com>
2016-12-28 20:24:57 +0800

    Add files via upload

--- GpenGL_test1/objects.cpp
+++ GpenGL_test1/objects.cpp
@@ -1,4 +1,7 @@
 #include "objects.h"
+#include <vector>
+
+using namespace std;
 
 # define leng_fac 0.1
 glm::vec3 axis_vertices[12]=
@@ -90,7 +93,7 @@ void cube_surface::initShader(std::string surfaceshader[],std::string elemshader
 	else if(elemshader2[1].length()==0)
 		_ele_color.initshader((GLchar *)elemshader1[0].c_str(),(GLchar *)elemshader1[1].c_str(),(GLchar *)elemshader1[2].c_str(),(GLchar *)elemshader2[0].c_str(),NULL,(GLchar *)elemshader2[2].c_str());
 	else
-		_ele_color.initshader((GLchar *)elemshader1[0].c_str(),(GLchar *)elemshader1[2].c_str(),(GLchar *)elemshader1[2].c_str(),(GLchar *)elemshader2[0].c_str(),(GLchar *)elemshader2[1].c_str(),(GLchar *)elemshader2[2].c_str());
+		_ele_color.initshader((GLchar *)elemshader1[0].c_str(),(GLchar *)elemshader1[1].c_str(),(GLchar *)elemshader1[2].c_str(),(GLchar *)elemshader2[0].c_str(),(GLchar *)elemshader2[1].c_str(),(GLchar *)elemshader2[2].c_str());
 }
 
 void cube_surface::loadvertex(glm::vec3 vex[])
@@ -122,9 +125,9 @@ void cube_surface::init(glm::vec3 vex[])
 	_surface.init(4,vertextmp, glm::vec3(1.0f, 0.5f, 0.2f),glm::mat4());
 	_surface.writebuffer();
 
-	_ele_color.init(glm::mat4(),SCALE);
+	_ele_color.init(glm::mat4(),SCALE,0.1,0.1);
 	GLfloat orign[] = {_vertex[2].x,_vertex[2].y,_vertex[2].z};
-	_ele_color.react_vertexchange(orign,_vertex[0]-_vertex[1],_vertex[1] - _vertex[2]);
+	_ele_color.react_vertexchange(orign,_vertex[0]-_vertex[1],_vertex[1] - _vertex[2],glm::vec3(_vertex[2].x,_vertex[2].y,_vertex[2].z + 1.0f) - _vertex[2]);
 }
 
 void cube_surface::draw(glm::mat4 const & model, glm::mat4 const & view, glm::mat4 const &projection)
@@ -149,7 +152,7 @@ void cube_surface::changevertex(glm::vec3 vex[])
 	_surface.changevertex(vertextmp);
 	GLfloat orign[] = {_vertex[2].x,_vertex[2].y,_vertex[2].z};
 
-	_ele_color.react_vertexchange(orign,_vertex[0]-_vertex[1],_vertex[1] - _vertex[2]);
+	_ele_color.react_vertexchange(orign,_vertex[0]-_vertex[1],_vertex[1] - _vertex[2],glm::vec3(_vertex[2].x,_vertex[2].y,_vertex[2].z + 1.0f) - _vertex[2]);
 }
 
 void cube_surface::moveedge(GLfloat incre, char axis, char dir)
@@ -196,5 +199,302 @@ void cube_surface::moveedge(GLfloat incre, char axis, char dir)
 	_surface.changevertex(vertextmp);
 	GLfloat orign[] = {_vertex[2].x,_vertex[2].y,_vertex[2].z};
 
-	_ele_color.react_vertexchange(orign,_vertex[0]-_vertex[1],_vertex[1] - _vertex[2]);
+	_ele_color.react_vertexchange(orign,_vertex[0]-_vertex[1],_vertex[1] - _vertex[2],glm::vec3(_vertex[2].x,_vertex[2].y,_vertex[2].z + 1.0f) - _vertex[2]);
+}
+
+
+
+GLuint _cube_ver_indices[] = {5,4,0,3,2,6,5,1,0,4,7,3,7,6,2,1};
+
+void cube::initShader(std::string surfaceshader[],std::string elemshader1[], std::string elemshader2[])
+{
+	if(surfaceshader[1].length()==0)
+		_surface.initshader((GLchar *)surfaceshader[0].c_str(),NULL,(GLchar * )surfaceshader[2].c_str());
+	else
+		_surface.initshader((GLchar *)surfaceshader[0].c_str(),(GLchar * )surfaceshader[1].c_str(),(GLchar * )surfaceshader[2].c_str());
+	
+	if(elemshader1[1].length()==0 && elemshader2[1].length()==0)
+		_ele_color.initshader((GLchar *)elemshader1[0].c_str(),NULL,(GLchar *)elemshader1[2].c_str(),(GLchar *)elemshader2[0].c_str(),NULL,(GLchar *)elemshader2[2].c_str());
+	else if(elemshader1[1].length()==0)
+		_ele_color.initshader((GLchar *)elemshader1[0].c_str(),NULL,(GLchar *)elemshader1[2].c_str(),(GLchar *)elemshader2[0].c_str(),(GLchar *)elemshader2[1].c_str(),(GLchar *)elemshader2[2].c_str());
+	else if(elemshader2[1].length()==0)
+		_ele_color.initshader((GLchar *)elemshader1[0].c_str(),(GLchar *)elemshader1[1].c_str(),(GLchar *)elemshader1[2].c_str(),(GLchar *)elemshader2[0].c_str(),NULL,(GLchar *)elemshader2[2].c_str());
+	else
+		_ele_color.initshader((GLchar *)elemshader1[0].c_str(),(GLchar *)elemshader1[1].c_str(),(GLchar *)elemshader1[2].c_str(),(GLchar *)elemshader2[0].c_str(),(GLchar *)elemshader2[1].c_str(),(GLchar *)elemshader2[2].c_str());
+}
+
+void cube::loadvertex(glm::vec3 vex[])
+{
+	for(int i =0; i < 8 ; i++)
+	{
+		_vertex[i] = vex[i];
+	}
+}
+
+void cube::changetype(GLuint rendertype)
+{
+	_ele_color.react_changetype(rendertype);
+}
+
+void cube::init(glm::vec3 vex[])
+{
+	loadvertex(vex);
+	_indicesnum = sizeof(_cube_ver_indices)/sizeof(GLuint); 
+	GLfloat * vertextmp = new GLfloat[3 * _indicesnum];
+	int k = 0;
+
+	for(int i =0; i<_indicesnum ; i++)
+	{
+		vertextmp[k++]=_vertex[_cube_ver_indices[i]].x;
+		vertextmp[k++]=_vertex[_cube_ver_indices[i]].y;
+		vertextmp[k++]=_vertex[_cube_ver_indices[i]].z;
+	}
+
+	_surface.init(_indicesnum,vertextmp, glm::vec3(1.0f, 0.5f, 0.2f),glm::mat4());
+
+	delete vertextmp;
+
+	_surface.writebuffer();
+
+	_ele_color.init(glm::mat4(),SCALE,0.1,0.1);
+	GLfloat orign[] = {_vertex[7].x,_vertex[7].y,_vertex[7].z};
+	_ele_color.react_vertexchange(orign,_vertex[4]-_vertex[7],_vertex[6]-_vertex[7],_vertex[3]-_vertex[7]);
+}
+
+void cube::draw(glm::mat4 const & model, glm::mat4 const & view, glm::mat4 const &projection)
+{
+	_surface.draw(model, view,projection);
+	_ele_color.draw(model,view,projection);
+}
+
+void cube::changevertex(glm::vec3 vex[])
+{
+	loadvertex(vex); 
+	GLfloat * vertextmp = new GLfloat[3 * _indicesnum];
+	int k = 0;
+
+	for(int i =0; i<_indicesnum ; i++)
+	{
+		vertextmp[k++]=_vertex[_cube_ver_indices[i]].x;
+		vertextmp[k++]=_vertex[_cube_ver_indices[i]].y;
+		vertextmp[k++]=_vertex[_cube_ver_indices[i]].z;
+	}
+
+	_surface.changevertex(vertextmp);
+
+	delete vertextmp;
+
+	GLfloat orign[] = {_vertex[7].x,_vertex[7].y,_vertex[7].z};
+
+	_ele_color.react_vertexchange(orign,_vertex[4]-_vertex[7],_vertex[6]-_vertex[7],_vertex[3]-_vertex[7]);
+}
+
+void cube::changecolor(glm::vec3 & color)
+{
+	_surface.changecolor(color);
+}
+int _cube_ver_tris[6][6]= 
+{
+	{0,1,2,0,3,2},
+	{4,5,6,6,7,4},
+	{0,1,5,5,4,0},
+	{2,3,7,7,6,2},
+	{1,2,6,6,5,1},
+	{0,3,7,7,4,0},
+};
+
+float cube::cal_incre(glm::vec2 const & move_vec,glm::mat4 const & model, glm::mat4 &view, glm::mat4 &projection, glm::vec4 &viewport, char flag)
+{
+	glm::vec3 dir;
+	float amp_factor = .8e-5;
+	switch (flag)
+	{
+	case 'x':
+		dir = glm::project(glm::vec3(1.0f,0.0f,0.0f),view*model,projection,viewport);
+		printf("incre: %f\n",glm::dot(move_vec,glm::vec2(dir.x,dir.y))*amp_factor);
+		return glm::dot(move_vec,glm::vec2(dir.x,dir.y))*amp_factor;
+		break;
+	case 'y':
+		dir = glm::project(glm::vec3(0.0f,1.0f,0.0f),view*model,projection,viewport);
+		printf("incre: %f\n",glm::dot(move_vec,glm::vec2(dir.x,dir.y))*amp_factor);
+		return glm::dot(move_vec,glm::vec2(dir.x,dir.y))*amp_factor;
+		break;
+	case 'z':
+		dir = glm::project(glm::vec3(0.0f,0.0f,1.0f),view*model,projection,viewport);
+		printf("incre: %f\n",glm::dot(move_vec,glm::vec2(dir.x,dir.y))*amp_factor);
+		return glm::dot(move_vec,glm::vec2(dir.x,dir.y))*amp_factor;
+		break;
+	default:
+		break;
+	}
+}
+
+void cube::move(glm::vec2 const & move_vec, glm::mat4 const & model, glm::mat4 & view, glm::mat4 & projection, glm::vec4 & viewport,int surface_index)
+{
+	float increment;
+	float dis_min = 0.4;
+	switch (surface_index)
+	{
+	case 0:
+		increment = cal_incre(move_vec, model,view,projection,viewport, 'z');
+		if(_vertex[_cube_ver_tris[0][0]].z + increment - _vertex[_cube_ver_tris[1][0]].z < dis_min) break;
+		_vertex[_cube_ver_tris[0][0]].z += increment;
+		_vertex[_cube_ver_tris[0][1]].z += increment;
+		_vertex[_cube_ver_tris[0][2]].z += increment;
+		_vertex[_cube_ver_tris[0][4]].z += increment;
+		break;
+	case 1:
+		increment = cal_incre(move_vec, model,view,projection,viewport, 'z');
+		if(_vertex[_cube_ver_tris[0][0]].z - increment - _vertex[_cube_ver_tris[1][0]].z < dis_min) break;
+		_vertex[_cube_ver_tris[1][0]].z += increment;
+		_vertex[_cube_ver_tris[1][1]].z += increment;
+		_vertex[_cube_ver_tris[1][2]].z += increment;
+		_vertex[_cube_ver_tris[1][4]].z += increment;
+		break;
+	case 2:
+		increment = cal_incre(move_vec, model,view,projection,viewport, 'x');
+		if(_vertex[_cube_ver_tris[2][0]].x + increment - _vertex[_cube_ver_tris[3][0]].x < dis_min) break;
+		_vertex[_cube_ver_tris[2][0]].x += increment;
+		_vertex[_cube_ver_tris[2][1]].x += increment;
+		_vertex[_cube_ver_tris[2][2]].x += increment;
+		_vertex[_cube_ver_tris[2][4]].x += increment;
+		break;
+	case 3:
+		increment = cal_incre(move_vec, model,view,projection,viewport, 'x');
+		if(_vertex[_cube_ver_tris[2][0]].x - increment - _vertex[_cube_ver_tris[3][0]].x < dis_min) break;
+		_vertex[_cube_ver_tris[3][0]].x += increment;
+		_vertex[_cube_ver_tris[3][1]].x += increment;
+		_vertex[_cube_ver_tris[3][2]].x += increment;
+		_vertex[_cube_ver_tris[3][4]].x += increment;
+		break;
+	case 4:
+		increment = cal_incre(move_vec, model,view,projection,viewport, 'y');
+		if(_vertex[_cube_ver_tris[4][0]].y + increment - _vertex[_cube_ver_tris[5][0]].y < dis_min) break;
+		_vertex[_cube_ver_tris[4][0]].y += increment;
+		_vertex[_cube_ver_tris[4][1]].y += increment;
+		_vertex[_cube_ver_tris[4][2]].y += increment;
+		_vertex[_cube_ver_tris[4][4]].y += increment;
+		break;
+	case 5:
+		increment = cal_incre(move_vec, model,view,projection,viewport, 'y');
+		if(_vertex[_cube_ver_tris[4][0]].y - increment - _vertex[_cube_ver_tris[5][0]].y < dis_min) break;
+		_vertex[_cube_ver_tris[5][0]].y += increment;
+		_vertex[_cube_ver_tris[5][1]].y += increment;
+		_vertex[_cube_ver_tris[5][2]].y += increment;
+		_vertex[_cube_ver_tris[5][4]].y += increment;
+		break;
+	default:
+		break;
+	}
+	changevertex(_vertex);
+}
+
+bool cube::get_intersec_triangle(glm::vec3 const & o, glm::vec3 const & dir, int vex_index[], float &t, glm::mat4 const & model)
+{
+	glm::vec3 V0 = _vertex[vex_index[0]];
+	glm::vec3 V1 = _vertex[vex_index[1]];
+	glm::vec3 V2 = _vertex[vex_index[2]];
+	glm::vec4 Vtmp;
+
+	Vtmp = model * glm::vec4(V0,1);
+	V0 = glm::vec3(Vtmp.x,Vtmp.y,Vtmp.z);
+
+	Vtmp = model * glm::vec4(V1,1);
+	V1 = glm::vec3(Vtmp.x,Vtmp.y,Vtmp.z);
+
+	Vtmp = model * glm::vec4(V2,1);
+	V2 = glm::vec3(Vtmp.x,Vtmp.y,Vtmp.z);
+
+
+	glm::vec3 edge1 = V1 - V0;
+	glm::vec3 edge2 = V2 - V0;
+	glm::vec3 pvec  = glm::cross(dir,edge2);
+
+	float det = glm::dot(edge1,pvec);
+
+	glm::vec3 tvec;
+	if(det > 0)
+	{
+		tvec = o - V0;
+	}
+	else
+	{
+		det  = -det;
+		tvec = V0 - o;
+	}
+
+	if(det < .0000f) return false;
+
+	float u = glm::dot(tvec, pvec);
+	if(u<0.0f || u>det) return false;
+
+	glm::vec3 qvec = glm::cross(tvec, edge1);
+	float v = glm::dot(dir, qvec);
+	if(v < 0.0f || (u + v) > det) return false;
+
+	t = glm::dot(edge2, qvec);
+
+	float fInvDet = 1.0f / det;
+	t *= fInvDet;
+		
+	return true;
+}
+
+bool cube::get_intersec(glm::vec3 const & o, glm::vec3 const & dir, int surface_index, float & t, glm::mat4 const & model)
+{
+	int vex_index[3];
+	for(int i = 0; i < 3;i++)
+	{
+		vex_index[i] = _cube_ver_tris[surface_index][i];
+	}
+	if(get_intersec_triangle(o, dir, vex_index, t, model))
+	{
+		return true;
+	}
+		
+	for(int i = 3; i < 6;i++)
+	{
+		vex_index[i - 3] = _cube_ver_tris[surface_index][i];
+	}
+	if(get_intersec_triangle(o, dir, vex_index, t, model))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool cube::find_insect_surface(glm::vec3 o, glm::vec3 dir, int & surface_index, glm::mat4 const & model)
+{
+	float len;
+	vector<surface_len> len_vec;
+	surface_len sur_tmp;
+
+	for(int i=0; i<6;i++)
+	{
+		if(get_intersec(o, dir, i, len, model))
+		{
+			sur_tmp.len = len;
+			sur_tmp.sur_index = i;
+			len_vec.push_back(sur_tmp);
+		}
+	}
+
+	if(len_vec.empty()) return false;
+
+	vector<surface_len>::iterator len_item;
+	len_item = len_vec.begin();
+	float tmp = len_item->len;
+	surface_index = len_item->sur_index;
+	len_item++;
+	for(;len_item != len_vec.end(); len_item++)
+	{
+		if(len_item->len < tmp)
+		{
+			tmp = len_item->len;
+			surface_index = len_item->sur_index;
+		}
+	}
+
+	return true;
 }
--- GpenGL_test1/objects.h
+++ GpenGL_test1/objects.h
@@ -28,7 +28,7 @@ class cube_surface
 private:
 	glm::vec3 _vertex[4];
 	gl_color_ver_primitive _surface;
-	gl_color_ver_instance_primitive _ele_color;
+	gl_color_ver_instance_3d_primitive _ele_color;
 public:
 	cube_surface(){};
 	cube_surface(glm::vec3 vertices[]);
@@ -41,6 +41,40 @@ public:
 	void changetype(GLuint rendertype);
 
 	void moveedge(GLfloat incre, char axis, char dir);
+};
+
+struct surface_len
+{
+	int sur_index;
+	float len;
+};
+
+
+class cube
+{
+private:
+	glm::vec3 _vertex[8];
+	gl_color_ver_primitive _surface;
+	gl_color_ver_instance_3d_primitive _ele_color;
+	GLuint _indicesnum;
+public:
+	cube(){};
+	//cube_surface(glm::vec3 vertices[]);
+	
+	void init(glm::vec3 vex[]);
+	void loadvertex(glm::vec3 vex[]);
+	void initShader(std::string surfaceshader[],std::string elemshader1[], std::string elemshader2[]);
+	void draw(glm::mat4 const & model, glm::mat4 const & view, glm::mat4 const &projection);
+	void changevertex(glm::vec3 vex[]);
+	void changetype(GLuint rendertype);
+	void changecolor(glm::vec3 & color);
+
+	float cal_incre(glm::vec2 const & move_vec,glm::mat4 const & model, glm::mat4 &view, glm::mat4 &projection, glm::vec4 &viewport, char flag);
+	void gene_gl_vertic(float *vertices, int surface_index, glm::vec3 & color);
+	bool get_intersec_triangle(glm::vec3 const & o, glm::vec3 const & dir, int vex_index[], float &t, glm::mat4 const & model);
+	bool get_intersec(glm::vec3 const & o, glm::vec3 const & dir, int surface_index, float & t, glm::mat4 const & model);
+	bool find_insect_surface(glm::vec3 o, glm::vec3 dir, int & surface_index, glm::mat4 const & model);
+	void move(glm::vec2 const & move_vec, glm::mat4 const & model, glm::mat4 & view, glm::mat4 & projection, glm::vec4 & viewport,int surface_index);
 };
 
 #endif
